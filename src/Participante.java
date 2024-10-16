40572e68dabb40fda8c4dc405d56092e3dfd441a
YoselynBascunan <Yoselyn.bascunan1201@gmail.com>
2024-10-16 13:33:38 -0300

    Initial commit

---
+++ src/Participante.java
@@ -0,0 +1,16 @@
+public class Participante {
+    private String nombre;
+
+    public Participante(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
